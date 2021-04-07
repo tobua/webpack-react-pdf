fed008b459ddd4c174efe8957a5ba919af763732
Matthias Giger <i@matthiasgiger.com>
2021-04-07 18:37:00 +0200

    Cleanup Setup and Update Dependencies for React-PDF 2 Refactoring

--- webpack.config.js
+++ webpack.config.js
@@ -1,37 +1,44 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 const CopyWebpackPlugin = require('copy-webpack-plugin')
 
-module.exports = env => ({
+module.exports = (env) => ({
   mode: env ? 'production' : 'development',
   module: {
     rules: [
       {
         test: /\.jsx?$/,
         loader: 'babel-loader',
-        exclude: /node_modules/
+        exclude: /node_modules/,
       },
       {
         test: /\.(png|jpg)$/,
-        loader: 'file-loader'
-      }
-    ]
+        loader: 'file-loader',
+      },
+    ],
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: 'src/index.html'
+      template: 'src/index.html',
     }),
-    new CopyWebpackPlugin([{
-      from: 'fonts/*', to: 'dist'
-    }])
+    new CopyWebpackPlugin({
+      patterns: [
+        {
+          from: 'fonts/*',
+          to: 'dist',
+        },
+      ],
+    }),
   ],
   devServer: {
-    open: true
+    open: true,
   },
   // Some libraries import Node modules but don't use them in the browser.
   // Tell Webpack to provide empty mocks for them so importing them works.
-  node: {
-    fs: 'empty',
-    net: 'empty',
-    tls: 'empty'
-  }
+  resolve: {
+    fallback: {
+      fs: false,
+      net: false,
+      tls: false,
+    },
+  },
 })
